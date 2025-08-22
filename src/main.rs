5515f087b2b4ace6f940c0d3a830535174ef6b44
ahlaiptek <iptekahla@gmail.com>
2025-08-22 11:01:52 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use clap::{ Parser, Subcommand };
+
+#[derive(Parser)]
+#[command(
+    author,
+    version,
+    about = "A PHP version manager inspired by Agritaloka.",
+    long_about = None
+)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Installs a specific version of PHP
+    Install {
+        /// The PHP version to install (e.g., 8.1.10)
+        version: String,
+    },
+    /// Sets the global PHP version to use
+    Use {
+        /// The PHP version to use
+        version: String,
+    },
+    /// Lists all installed PHP versions
+    List,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Install { version } => {
+            println!("Installing PHP version: {}", version);
+            // TODO: Add installation logic here
+        }
+        Commands::Use { version } => {
+            println!("Using PHP version: {}", version);
+            // TODO: Add logic to switch versions here
+        }
+        Commands::List => {
+            println!("List of installed PHP versions:");
+            // TODO: Add logic to list versions here
+        }
+    }
 }
